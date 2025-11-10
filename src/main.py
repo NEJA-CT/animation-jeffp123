ca4189a9d626e64c863c0b51c59def9e27f3b7a7
Jeff Peck <chaimpeck@gmail.com>
2025-11-10 01:12:02 +0000

    spiral with lines

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import pyxel
 import math
 
-W, H = 160, 120
+W, H = 320, 240
 cx, cy = W // 2, H // 2
+
+last_x, last_y = cx, cy
 
 angle = 0.0
 radius = 0.0
@@ -16,11 +18,14 @@ def update():
         angle = 0
 
 def draw():
-    # pyxel.cls(0)
+    global last_x, last_y
 
     x = cx + radius * math.cos(angle)
     y = cy + radius * math.sin(angle)
-    pyxel.pset(x, y, 7)
+    pyxel.line(x, y, last_x, last_y, 7)
+
+    last_x = x
+    last_y = y
 
 pyxel.init(W, H, title="Pyxel Spiral Art")
 pyxel.run(update, draw)
